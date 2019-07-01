bf6813302666073b91bf5cfdb98d778f57d84dce
Рома Баранников <gagarin40000@gmail.com>
2019-07-02 00:04:26 +0300

    Типы данных

--- common.js
+++ common.js
@@ -1,4 +1,7 @@
-var leftBorderWidth = 1;
-let second = 2;
-const pi = 3.14;
-console.log(second);
+var number = 5;
+var string = "hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
