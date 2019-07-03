de918503ee9db7498a708fa6f2f4c18f4dff33cb
Рома Баранников <gagarin40000@gmail.com>
2019-07-03 08:25:49 +0300

    типы данных

--- common.js
+++ common.js
@@ -5,3 +5,23 @@ var boolean = true;
 null;
 undefined;
 var obj = {};
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+
+console.log(persone.name);
+console.log(persone["age"]);
+
+let arr = ['plam.png','orange.jpb','apple.bmp'];
+
+console.log(arr[0]);
+
+// let answer = confirm('miss me?');
+// console.log(answer);
+
+let answer = prompt('есть ли вам 18?', 'да');
+
+console.log(typeof(null));
