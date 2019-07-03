43951d3d43e3f6382959acf124b81fcf46ffb545
Рома Баранников <gagarin40000@gmail.com>
2019-07-03 17:52:49 +0300

    Начал делать первое задание

--- common.js
+++ common.js
@@ -1,3 +1,9 @@
+'use strict';
+
+a = 5;
+
+console.log(a);
+
 var number = 5;
 var string = "hello";
 var sym = Symbol();
@@ -12,16 +18,32 @@ let persone = {
     isMarried: false
 }
 
-console.log(persone.name);
-console.log(persone["age"]);
+// console.log(persone.name);
+// console.log(persone["age"]);
 
-let arr = ['plam.png','orange.jpb','apple.bmp'];
+// let arr = ['plam.png','orange.jpb','apple.bmp'];
 
-console.log(arr[0]);
+// console.log(arr[0]);
 
-// let answer = confirm('miss me?');
-// console.log(answer);
+// // let answer = confirm('miss me?');
+// // console.log(answer);
 
-let answer = prompt('есть ли вам 18?', 'да');
+// // let answer = prompt('есть ли вам 18?', 'да');
 
-console.log(typeof(null));
+// // console.log(typeof(null));
+// console.log(typeof(+'4'+ 4));
+// console.log(+'4'+ 4);
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+console.log(5%2);
+
+console.log('2' == 2);
+console.log('2' === 2);
+
+let isChecked = false,
+    isClose = true;
+
+console.log(isChecked || !isClose);
